2c71641e93e1ff3ac22c0c373bae22b69963670e
Vyshnavi1410 <vyshnavikattamuri14@gmail.com>
2022-11-01 12:30:38 -0500

    added new endpoint selector

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var hallRouter = require('./routes/hall');
 var gridbuildRouter = require('./routes/gridbuild');
-
+var selectorRouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -25,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hall',hallRouter);
 app.use('/gridbuild',gridbuildRouter);
+app.use('/selector',selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
